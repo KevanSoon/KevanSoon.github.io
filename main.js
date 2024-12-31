1c01ff5b9b2dd666c3181e7ccd8317888c36c57e
KevanSoon <kevansoon@gmail.com>
2024-12-31 16:52:45 +0800

    Revamp Portfolio Website

---
+++ main.js
@@ -0,0 +1,9 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['an Undergraduate in Software Engineering','a Card Collector','a Gamer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+});
+
+
